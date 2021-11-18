f2bf322c9a5e5eaa4b8864f9f339fc47906f6fc7
atif00 <mohammedatif90@gmail.com>
2021-11-18 20:08:16 +0530

    Added: REST call to say hello with name Using Get method and passing name as path variable

--- src/main/java/com/example/demo/controller/RestApiController.java
+++ src/main/java/com/example/demo/controller/RestApiController.java
@@ -5,10 +5,13 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@org.springframework.web.bind.annotation.RestController
+
+@RestController
 @RequestMapping("/hello")
-public class RestController {
+public class RestApiController {
     @RequestMapping(value = {"", "/", "home"})
     public String sayHello() {
         return "Hello From Bridgelabz..";
@@ -20,7 +23,7 @@ public class RestController {
     }
 
     @GetMapping("/param/{name}")
-    public String sayHelloPara(@PathVariable String name) {
+    public String sayHelloParam(@PathVariable String name) {
         return "Hello " + name + " From Bridgelabz";
     }
 }
